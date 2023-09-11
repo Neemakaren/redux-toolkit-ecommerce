86cc497a0247cfb0cfca129543ae57bde7119bc1
Neema <bun4339@gmail.com>
2023-09-11 14:26:50 +0300

    first commit

---
+++ redux-toolkit/src/components/Error/Error.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Error.scss";
+
+
+const Error = () => {
+  return (
+    <div className='container'>
+        <div class = "flex flex-center error">
+            Page Not Found
+        </div>
+    </div>
+  )
+}
+
+export default Error
